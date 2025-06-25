6a231927eeb7543b033e5886bd93d360f7ac555d
TatyanaZakiryanova <elenastrell@yandex.ru>
2025-06-25 16:37:54 +0300

    add auth guard

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { UsersModule } from 'src/users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule,
+    UsersModule,
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: config.get<string>('JWT_EXPIRES_IN') },
+      }),
+    }),
+  ],
+})
+export class AuthModule {}
